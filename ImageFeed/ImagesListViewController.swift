3afff0bb9a6ab52b8d332438922a002109eaeed0
milhani <haninamila@mail.ru>
2024-01-17 17:03:41 +0300

    Работа над таблицей №1

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -1,12 +1,24 @@
 import UIKit
 
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
 class ImagesListViewController: UIViewController {
 
     @IBOutlet private var tableView: UITableView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
