9679a7033831bfbe56f05727a3840ed0b40abd9b
milhani <haninamila@mail.ru>
2024-02-02 18:46:17 +0300

    Добавление экрана профиля

---
+++ Image Feed/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Image Feed
+//
+//  Created by Людмила Ханина on 02.02.2024.
+//
+
+import SwiftUI
+
+struct ProfileViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileViewController()
+}
