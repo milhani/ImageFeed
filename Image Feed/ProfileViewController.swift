5df9f0bb4a9e21acd2eaaf19ea3a699049217a92
milhani <haninamila@mail.ru>
2024-02-02 18:46:17 +0300

    Добавление экрана профиля

---
+++ Image Feed/ProfileViewController.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImage: UIImageView!
+    
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var nameLabel: UIImageView!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton() {
+    }
+}
