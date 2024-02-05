343c76b8863dcc3e761f2cb2684c2c0f202c8a82
milhani <haninamila@mail.ru>
2024-02-05 20:26:06 +0300

    Сделала увелечение изображения на весь экран. Ура!

--- Image Feed/SingleImageViewController.swift
+++ Image Feed/SingleImageViewController.swift
@@ -12,7 +12,11 @@ final class SingleImageViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet private var imageView: UIImageView!
     
-
+    @IBOutlet weak var imageConstraintBottom: NSLayoutConstraint!
+    @IBOutlet weak var imageConstraintLeft: NSLayoutConstraint!
+    @IBOutlet weak var imageConstraintTop: NSLayoutConstraint!
+    @IBOutlet weak var imageConstraintRight: NSLayoutConstraint!
+    
     @IBAction func didTapBackButton() {
         dismiss(animated: true, completion: nil)
     }
@@ -20,9 +24,10 @@ final class SingleImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.minimumZoomScale = 0.1
-        scrollView.maximumZoomScale = 1.25
+        scrollView.maximumZoomScale = 8 //1.25
         imageView.image = image
         rescaleAndCenterImageInScrollView(image: image)
+        print("didload")
     }
     @IBAction func didTapShareButton(_ sender: Any) {
         let share = UIActivityViewController(
@@ -33,20 +38,35 @@ final class SingleImageViewController: UIViewController {
     }
     
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
-        let minZoomScale = scrollView.minimumZoomScale
-        let maxZoomScale = scrollView.maximumZoomScale
-        view.layoutIfNeeded()
-        let visibleRectSize = scrollView.bounds.size
-        let imageSize = image.size
-        let hScale = visibleRectSize.width / imageSize.width
-        let vScale = visibleRectSize.height / imageSize.height
-        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
-        scrollView.setZoomScale(scale, animated: false)
-        scrollView.layoutIfNeeded()
-        let newContentSize = scrollView.contentSize
-        let x = (newContentSize.width - visibleRectSize.width) / 2
-        let y = (newContentSize.height - visibleRectSize.height) / 2
-        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+        if let image = imageView.image {
+            let zoom = max(scrollView.bounds.size.width / image.size.width,
+                              scrollView.bounds.size.height / image.size.height)
+            print(zoom)
+            scrollView.setZoomScale(zoom, animated: false)
+            scrollView.layoutIfNeeded()
+        }
+    }
+    
+    func updateConstraints() {
+        if let image = imageView.image {
+            let viewWidth = scrollView.bounds.size.width
+            let viewHeight = scrollView.bounds.size.height
+            
+            let x = (viewWidth - scrollView.zoomScale * image.size.width) / 2
+            let y = (viewHeight - scrollView.zoomScale * image.size.height) / 2
+            
+            imageConstraintLeft.constant = x
+            imageConstraintRight.constant = x
+            
+            imageConstraintTop.constant = y
+            imageConstraintBottom.constant = y
+            
+            view.layoutIfNeeded()
+        }
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        updateConstraints()
     }
 }
 
