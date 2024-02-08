552248f7996ba241fa4bef207acfbc6750b901b0
milhani <haninamila@mail.ru>
2024-02-08 14:26:09 +0300

    Добавление Constants

---
+++ Image Feed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "-UeNe21wxANS-QcW9DsJhEZk-6Z-UNHnbRpgnwtGSf0"
+let SecretKey = "OTtoNu-cY4tOAOZ8QR2E2zpITR0HCv-F7IXinOUR-Bg"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
