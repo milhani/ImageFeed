194af72644cdcc2130de3e773861a50bc5bb3ff1
Khanina Mila <72822370+milhani@users.noreply.github.com>
2024-03-06 23:00:52 +0300

    Update UIColor+Extensions.swift

--- Image Feed/Helpers/UIColor+Extensions.swift
+++ Image Feed/Helpers/UIColor+Extensions.swift
@@ -1,8 +1,10 @@
-//
-//  UIColor+Extensions.swift
-//  Image Feed
-//
-//  Created by Людмила Ханина on 06.03.2024.
-//
+import UIKit
 
-import Foundation
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
+    static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+}
