28b348483aeb8269f3ba29827cef521a48bcf229
Khanina Mila <72822370+milhani@users.noreply.github.com>
2024-03-06 22:58:58 +0300

    Update OAuth2Service.swift

--- Image Feed/Auth/OAuth2Service.swift
+++ Image Feed/Auth/OAuth2Service.swift
@@ -1,8 +1,91 @@
-//
-//  OAuth2Service.swift
-//  Image Feed
-//
-//  Created by Людмила Ханина on 09.02.2024.
-//
-
 import Foundation
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+    public let urlSession = URLSession.shared
+    private (set) var authToken: String? {
+        get {
+            return OAuth2TokenStorage().token
+        }
+        set {
+            OAuth2TokenStorage().token = newValue
+        }
+    }
+    
+    private func authTokenRequest(code: String) -> URLRequest? {
+        guard let url = URL(string: "https://unsplash.com") else { return nil }
+        return URLRequest.makeHTTPRequest(
+            path: "/oauth/token"
+            + "?client_id=\(AccessKey)"
+            + "&&client_secret=\(SecretKey)"
+            + "&&redirect_uri=\(RedirectURI)"
+            + "&&code=\(code)"
+            + "&&grant_type=authorization_code",
+            httpMethod: "POST",
+            baseURL: url)
+    }
+    
+    func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let request = authTokenRequest(code: code) else { return }
+        
+        let task = object(for: request) { [weak self] result in
+                        guard let self = self else { return }
+                        switch result {
+                        case .success(let body):
+                            let authToken = body.accessToken
+                            self.authToken = authToken
+                            completion(.success(authToken))
+                        case .failure(let error):
+                            completion(.failure(error))
+                        }
+                    }
+        task.resume()
+    }
+    
+    private func object(for request: URLRequest,
+                        completion: @escaping (Result<OAuthTokenResponseBody, Error>) -> Void) -> URLSessionTask {
+        let decoder = JSONDecoder()
+        
+        return urlSession.data(for: request) { (result: Result<Data, Error>) in
+            let response = result.flatMap { data -> Result<OAuthTokenResponseBody, Error> in
+                Result {
+                    try decoder.decode(OAuthTokenResponseBody.self, from: data)
+                }
+            }
+            completion(response)
+        }
+    }
+}
+
+extension OAuth2Service {
+    
+    private struct OAuthTokenResponseBody: Decodable {
+        let accessToken: String
+        let tokenType: String
+        let scope: String
+        let createdAt: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case accessToken = "access_token"
+            case tokenType = "token_type"
+            case scope
+            case createdAt = "created_at"
+        }
+    }
+}
+
+extension URLRequest {
+    static func makeHTTPRequest(
+        path: String,
+        httpMethod: String,
+        baseURL: URL = DefaultBaseURL
+    ) -> URLRequest? {
+        guard let url = URL(string: path, relativeTo: baseURL)
+        else {
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod
+        return request
+    }
+}
