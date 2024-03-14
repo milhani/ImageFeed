49e58df27bbd5cefdf1c0b39cd22752c29b1a88e
milhani <haninamila@mail.ru>
2024-03-14 21:48:31 +0300

    Create ProfileResult

---
+++ Image Feed/Profile/ProfileResult.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileResult.swift
+//  Image Feed
+//
+//  Created by Людмила Ханина on 14.03.2024.
+//
+
+import Foundation
--- Image Feed/Profile/ProfileService.swift
+++ Image Feed/Profile/ProfileService.swift
@@ -1,8 +1,3 @@
-//
-//  ProfileService.swift
-//  Image Feed
-//
-//  Created by Людмила Ханина on 14.03.2024.
-//
-
 import Foundation
+
+final class ProfileService { }
