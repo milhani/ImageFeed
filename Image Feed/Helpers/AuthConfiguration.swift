fbab441314d3f1e98b7e8af138f59d7edbbac87d
milhani <haninamila@mail.ru>
2024-03-30 16:16:36 +0300

    Перенос ответственности из WebViewController'а

---
+++ Image Feed/Helpers/AuthConfiguration.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "-UeNe21wxANS-QcW9DsJhEZk-6Z-UNHnbRpgnwtGSf0"
+    static let secretKey = "OTtoNu-cY4tOAOZ8QR2E2zpITR0HCv-F7IXinOUR-Bg"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
+struct AuthConfiguration {
+    let accessKey: String
+    let secretKey: String
+    let redirectURI: String
+    let accessScope: String
+    let defaultBaseURL: URL
+    let authURLString: String
+
+    init(accessKey: String, secretKey: String, redirectURI: String, accessScope: String, authURLString: String, defaultBaseURL: URL) {
+        self.accessKey = accessKey
+        self.secretKey = secretKey
+        self.redirectURI = redirectURI
+        self.accessScope = accessScope
+        self.defaultBaseURL = defaultBaseURL
+        self.authURLString = authURLString
+    }
+    
+    static var standard: AuthConfiguration {
+        return AuthConfiguration(accessKey: Constants.accessKey,
+                                 secretKey: Constants.secretKey,
+                                 redirectURI: Constants.redirectURI,
+                                 accessScope: Constants.accessScope,
+                                 authURLString: Constants.unsplashAuthorizeURLString,
+                                 defaultBaseURL: Constants.defaultBaseURL)
+    }
+}
--- Image Feed/Helpers/Constants.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum Constants {
-    static let accessKey = "-UeNe21wxANS-QcW9DsJhEZk-6Z-UNHnbRpgnwtGSf0"
-    static let secretKey = "OTtoNu-cY4tOAOZ8QR2E2zpITR0HCv-F7IXinOUR-Bg"
-    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
-}
